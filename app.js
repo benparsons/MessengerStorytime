d5b748428572d5267649e316768075573a104f30
Ben Parsons <ben@bpulse.co.uk>
2017-04-19 21:21:16 +0100

    remove postback (state) indicators and add ending

--- app.js
+++ app.js
@@ -354,6 +354,10 @@ function sendState(recipientId, stateIndex, previousPart) {
     sendSendStateListButtons(recipientId, stateIndex);
   }
 
+  if (previousPart === "lasttext") {
+    sendStateTextMessage(recipientId, "THE END", stateIndex + ",end");
+  }
+
 }
 
 
@@ -404,7 +408,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called: " + payload);
+  //sendTextMessage(senderID, "Postback called: " + payload);
   sendState(senderID, payload);
 }
 
