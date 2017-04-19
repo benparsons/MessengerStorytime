c8cd23124d70c3d0f36eca048e8282ccd97320bc
Ben Parsons <ben@bpulse.co.uk>
2017-04-19 17:49:08 +0100

    update the csv loader with new format

--- csv-loader.js
+++ csv-loader.js
@@ -6,7 +6,8 @@ var parse = require('csv-parse');
 var byColumn = [];
 var results = {};
 
-fs.createReadStream('murder1.csv')
+
+fs.createReadStream(process.argv[2])
     .pipe(parse({delimiter: ','}))
     .on('data', function(csvrow) {
         for (var i = 1; i < csvrow.length; i++) {
@@ -21,16 +22,19 @@ fs.createReadStream('murder1.csv')
       for (var i in byColumn) {
         var sceneArray = byColumn[i];
         var sceneObj = {};
-        sceneObj['text'] = sceneArray[1];
+        if (sceneArray[1]) {
+          sceneObj['image'] = sceneArray[1];
+        }
+        sceneObj['text'] = sceneArray[2];
         var responses = [];
-        if (sceneArray[2] && sceneArray[3]) {
-          responses.push({"text": sceneArray[2], "link": sceneArray[3]});
+        if (sceneArray[3] && sceneArray[4]) {
+          responses.push({"text": sceneArray[3], "link": sceneArray[4]});
         }
-        if (sceneArray[4] && sceneArray[5]) {
-          responses.push({"text": sceneArray[4], "link": sceneArray[5]});
+        if (sceneArray[7] && sceneArray[8]) {
+          responses.push({"text": sceneArray[7], "link": sceneArray[8]});
         }
-        if (sceneArray[6] && sceneArray[6]) {
-          responses.push({"text": sceneArray[6], "link": sceneArray[7]});
+        if (sceneArray[11] && sceneArray[12]) {
+          responses.push({"text": sceneArray[11], "link": sceneArray[12]});
         }
         sceneObj['responses'] = responses;
         results[sceneArray[0]] = sceneObj;
